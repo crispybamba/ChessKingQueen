b3f35ac40722160b35c10f92657d577e38c40599
crispybamba <michaelsh2006@gmail.com>
2022-03-03 20:17:08 +0200

    working with one queen only the queen is placed randomly placed and can be moved anywhere

--- ChessKingQueen/Program.cs
+++ ChessKingQueen/Program.cs
@@ -280,7 +280,7 @@ namespace ChessQueens
 
                     if (gameBoard[row, column] != empty)
                     {
-                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.Write(gameBoard[row, column] + "\t");
                         Console.ForegroundColor = ConsoleColor.DarkGreen;
                     }
